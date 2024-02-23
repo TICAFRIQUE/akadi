0b0a04878e13da39ef84766c416d158cb6074281
davyalex <alexkouamelan96pv@gmail.com>
2024-02-23 17:41:58 +0000

    optimize

--- resources/views/site/sections/pub/video_and_facebook.blade.php
+++ resources/views/site/sections/pub/video_and_facebook.blade.php
@@ -5,8 +5,12 @@
             <div class="container">
                 <div class="video-box1">
 
-                    <iframe width="420" height="315" src="https://www.youtube.com/shorts/C2zjRbosJ10">
-                    </iframe>
+
+                    <iframe width="640" height="360" src="https://www.youtube.com/embed/QH55W44Tx0w"
+                        title="TASTY CHICKEN MASALA FRY | CHATPATA CHICKEN FRY RECIPE | CHICKEN FRY" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen></iframe>
+
                     {{-- <img src="{{ asset('site/assets/img/normal/video_1.jpg') }}" alt="video" />
                     <div class="video-content">
                         <h2 class="h4 text-white font-style">
@@ -15,6 +19,10 @@
                         <a href="https://www.youtube.com/shorts/C2zjRbosJ10" class="play-btn popup-video"><i
                                 class="fas fa-play"></i></a>
                     </div> --}}
+                    <div class="text-center text-xl-center">
+                        <a href="{{ route('liste-produit') }}" class="th-btn rounded-2">Voir plus de videos<i
+                                class="fa-solid fa-arrow-right ms-2"></i></a>
+                    </div>
                 </div>
             </div>
             <div class="shape-mockup leaf jump-reverse d-none d-md-block" data-top="10%" data-left="2%">
