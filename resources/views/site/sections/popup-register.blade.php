8dc7d881be953a1cbf6fd23884638fcee9f819e4
davyalex <alexkouamelan96pv@gmail.com>
2024-03-23 20:09:29 +0000

    optimize

--- resources/views/site/sections/popup-register.blade.php
+++ resources/views/site/sections/popup-register.blade.php
@@ -1,5 +1,5 @@
 <style>
-    .card {
+    /* .card {
         overflow: hidden;
         position: relative;
         text-align: left;
@@ -7,27 +7,9 @@
         max-width: 400px;
         box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04);
         background-color: #fff;
-    }
+    } */
 
-    .dismiss {
-        position: absolute;
-        right: 10px;
-        top: 10px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        /* padding: 0.5rem 1rem; */
-        background-color: #fff;
-        color: black;
-        border: 2px solid #f88808;
-        font-size: 1rem;
-        font-weight: 300;
-        width: 15px;
-        height: 15px;
-        border-radius: 7px;
-        transition: .3s ease;
-    }
-
+  
     .dismiss:hover {
         background-color: #f85d05;
         border: 2px solid #f85d05;
@@ -127,45 +109,39 @@
 
 
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-sm-6 col-xs-6 m-auto">
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-                aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header border-0">
-                            <h5 class="modal-title" id="staticBackdropLabel">
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header border-0">
+                <h5 class="modal-title" id="staticBackdropLabel">
 
-                            </h5>
-                            <button type="button" class="btn-close dismiss p-2" data-bs-dismiss="modal" aria-label="Close">
-                                x</button>
-                        </div>
-                        <div class="modal-body border-top-0">
-                            <div class="image mb-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                    <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
-                                    <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier">
-                                    </g>
-                                    <g id="SVGRepo_iconCarrier">
-                                        <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5"
-                                            stroke="#000000" d="M20 7L9.00004 18L3.99994 13"></path>
-                                    </g>
-                                </svg>
-                            </div>
-                            <span class="title text-justify"> Crée ton compte et bénéficie de la livraison gratuite sur tes prochaines
-                                commandes
-                            </span>
-                        </div>
-                        <div class="modal-footer border-0">
-                            <a href="{{ route('register-form') }}"
-                                style="background-color:#f85d05; border-color:#f85d05" role="button"
-                                class="btn btn-secondary">Je m'inscris</a>
-                            <button type="button" class="btn btn-dark" data-bs-dismiss="modal">J'ai déjà un
-                                compte</button>
-                        </div>
-                    </div>
-                </div>
+                </h5>
+                <button type="button" class="btn-close dismiss p-2" data-bs-dismiss="modal" aria-label="Close">
+                    x</button>
+            </div>
+            <div class="modal-body border-top-0">
+                {{-- <div class="image mb-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                        <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier">
+                        </g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#000000"
+                                d="M20 7L9.00004 18L3.99994 13"></path>
+                        </g>
+                    </svg>
+                </div> --}}
+                <span class="title text-justify"> Crée ton compte et bénéficie de la livraison gratuite sur tes
+                    prochaines
+                    commandes
+                </span>
+            </div>
+            <div class="modal-footer border-0">
+                <a href="{{ route('register-form') }}" style="background-color:#f85d05; border-color:#f85d05"
+                    role="button" class="btn btn-secondary">Je m'inscris</a>
+                <button type="button" class="btn btn-dark" data-bs-dismiss="modal">J'ai déjà un
+                    compte</button>
             </div>
         </div>
     </div>
