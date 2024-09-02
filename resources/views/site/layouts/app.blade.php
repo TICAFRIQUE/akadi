e896a7c9f88c59a3e6f579df79b83e3a14b85f16
davyalex <alexkouamelan96pv@gmail.com>
2024-09-02 09:39:27 +0000

    optimize

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -4,24 +4,17 @@
 <head>
 
     <!-- ========== Start google analytics ========== -->
-    <!-- Google Tag Manager -->
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ELMED1GRWL"></script>
     <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-KLFF786Q');
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-ELMED1GRWL');
     </script>
-    <!-- End Google Tag Manager -->
     <!-- ========== End google analytics ========== -->
 
 
@@ -84,7 +77,7 @@
 <body>
 
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KLFF786Q" height="0" width="0"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-ELMED1GRWL" height="0" width="0"
             style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
