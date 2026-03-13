9e081576c901f0e64136d67da9a14772ad7c0d6b
davyalex <alexkouamelan96pv@gmail.com>
2026-03-13 09:05:20 +0000

    ajout de nouvelle card pour les nouvelles commandes

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -42,7 +42,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Favicons - Place favicon.ico in the root directory -->
-    <link rel="shortcut icon" href="{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('admin/assets/img/favicon/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32"
         href="{{ asset('admin/assets/img/favicon/favicon-32x32.png') }}">
