665f10ada5ae53823a08d460127712da9ea3ccb6
davyalex <alexkouamelan96pv@gmail.com>
2026-03-17 08:37:51 +0000

    optimize affichage categorie

--- resources/views/site/sections/categorie.blade.php
+++ resources/views/site/sections/categorie.blade.php
@@ -5,16 +5,19 @@
             <div class="title-area text-center">
                 <span class="sub-title">Menu</span>
                 <h2 class="sec-title">Parcourir nos catégories <span class="font-style text-theme">Akadi</span></h2>
+                <p class="sec-text d-md-none" style="font-size: 14px; color: #666; margin-top: 10px;">
+                    <i class="fas fa-hand-point-left"></i> Glissez pour voir plus de catégories <i class="fas fa-hand-point-right"></i>
+                </p>
             </div>
             <div class="row th-carousel" data-slide-show="4" data-ml-slide-show="4" data-lg-slide-show="4"
-                data-md-slide-show="4" data-sm-slide-show="2" data-xs-slide-show="2">
+                data-md-slide-show="3" data-sm-slide-show="2" data-xs-slide-show="1.5" data-arrows="true" data-dots="true">
                @foreach ($categories as $item)
-                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm2">
+                     <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
                     <div class="category-border">
                         <div class="category-border_img">
                             <a href="/produit?categorie={{$item['id']}}">
                                 <img src="{{ $item->getFirstMediaUrl('category_image') }}"
-                                alt="{{ $item->getFirstMediaUrl('category_image') }}">
+                                alt="{{ $item->getFirstMediaUrl('category_image') }}" class="img-fluid">
                             </a>
                         </div>
                         
